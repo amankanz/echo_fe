951bbf4324fe76629d4a69c837070059a3c3303b
Amani Kaneza Fred <95358548+amankanz@users.noreply.github.com>
2025-11-20 12:05:35 +0300

    Initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,11 @@
+//src/app/index.tsx
+import { View, Text } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View>
+      <Text>Hello, Nuru here!</Text>
+      <Text>Welcome, on the Native Echo!</Text>
+    </View>
+  );
+}
